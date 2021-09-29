7e3c6bd06a2407232e7c717db87966b5ed4bb566
Dhananjay <dhananjayman11@gmail.com>
2021-09-29 12:34:13 +0530

    room component header

--- client/src/components/pages/Rooms/Rooms.js
+++ client/src/components/pages/Rooms/Rooms.js
@@ -2,11 +2,30 @@ import React from 'react'
 import styles from './Rooms.module.css'
 
 const Rooms = () => {
-    
+
     return (
         <div>
-            Rooms Component
-            
+            <>
+                <div className="container">
+                    <div className={styles.topBar}>
+                        <div className={styles.left}>
+                            <span className={styles.heading}>Chat Rooms</span>
+                            <div className={styles.search}>
+                                <i class="fas fa-search"></i>
+                                <input className={styles.searchInput} type="text" />
+                            </div>
+                        </div>
+                        <div className={styles.right}>
+                            <button className={styles.startRoomBtn}>
+                                <i class="fas fa-microphone"></i>
+                                <span className={styles.roomBtnText}>Start a room</span>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </>
+
         </div>
     )
 }

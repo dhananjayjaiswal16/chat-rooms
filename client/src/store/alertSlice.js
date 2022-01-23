5c1ee738392dddcb20c0e6418cdc8814992f3bc7
Dhananjay <dhananjayman11@gmail.com>
2022-01-23 23:29:22 +0530

    chore fix

--- client/src/store/alertSlice.js
+++ client/src/store/alertSlice.js
@@ -1,26 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const alertSlice = createSlice({
-    name: 'alert',
+  name: 'alert',
 
-    initialState: {
-        alerts: []
+  initialState: {
+    alerts: []
+  },
+
+  reducers: {
+    setAlertMsg: (state, action) => {
+      console.log("action.payload in set", action.payload);
+      state.alerts.push(action.payload);
     },
-
-    reducers: {
-        setAlertMsg: (state, action) => {
-            console.log("action.payload in set", action.payload);
-            state.alerts.push(action.payload);
-        },
-        removeAlertMsg: (state, action) => {
-            console.log("action.payload in remove", action.payload);
-            state.alerts = state.alerts.filter(alert =>
-                alert.id !== action.payload
-            );
-
-
-        }
+    removeAlertMsg: (state, action) => {
+      console.log("action.payload in remove", action.payload);
+      state.alerts = state.alerts.filter(alert =>
+        alert.id !== action.payload
+      );
     }
+  }
 })
 
 // Action creators are generated for each case reducer function

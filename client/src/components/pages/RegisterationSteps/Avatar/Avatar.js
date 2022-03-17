40bcb765f27782e962a93dd19d714e9bff07506e
Dhananjay <dhananjayman11@gmail.com>
2022-03-17 12:28:40 +0530

    class error fix

--- client/src/components/pages/RegisterationSteps/Avatar/Avatar.js
+++ client/src/components/pages/RegisterationSteps/Avatar/Avatar.js
@@ -19,82 +19,82 @@ import { setAuth } from '../../../../store/authSlice'
 
 const Avatar = ({ onClick }) => {
 
-    const { name, avatar } = useSelector(state => state.activateSlice);
-    const [image, setImage] = useState('/react-logo.png');
-    const [loading, setLoading] = useState(false);
-    const [unMounted, setUnMounted] = useState(false);
+  const { name, avatar } = useSelector(state => state.activateSlice);
+  const [image, setImage] = useState('/react-logo.png');
+  const [loading, setLoading] = useState(false);
+  const [unMounted, setUnMounted] = useState(false);
 
-    const dispatch = useDispatch();
-    const id = uuidv4();
-    const setSelectedImage = (e) => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-        reader.readAsDataURL(file);//this step takes some time to execute
-        reader.onloadend = () => {
-            setImage(reader.result);
-            dispatch(setAvatar(reader.result))
-        }
+  const dispatch = useDispatch();
+  const id = uuidv4();
+  const setSelectedImage = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);//this step takes some time to execute
+    reader.onloadend = () => {
+      setImage(reader.result);
+      dispatch(setAvatar(reader.result))
     }
-    const submit = async () => {
-        if (!name || !avatar) {
-            dispatch(setAlertMsg({ msg: 'Profile Picture is required', id: id }))
+  }
+  const submit = async () => {
+    if (!name || !avatar) {
+      dispatch(setAlertMsg({ msg: 'Profile Picture is required', id: id }))
 
 
-            setTimeout(() => (
-                dispatch(removeAlertMsg(id))
-            ), 3500);
+      setTimeout(() => (
+        dispatch(removeAlertMsg(id))
+      ), 3500);
 
-            return;
-        }
-        setLoading(true);
-        try {
-            const { data } = await activate({ name, avatar });
-            if (data.auth) {
-                if (!unMounted) {
-                    dispatch(setAuth(data));
-                }
-            }
-            setLoading(false);
-        } catch (err) {
-            console.log(err);
-            setLoading(false);
-        }
+      return;
     }
-
-    useEffect(() => {
-        //Runs when component unmounts
-        return () => {
-            setUnMounted(true);
+    setLoading(true);
+    try {
+      const { data } = await activate({ name, avatar });
+      if (data.auth) {
+        if (!unMounted) {
+          dispatch(setAuth(data));
         }
-    }, []);
-
-
-    if (loading) {
-        return <Loader msg='Authentication in progress' />
+      }
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
     }
-    return (
-        <div className={styles.cardContainer}>
-            <Card emoji="😎" title={`Hey, ${name}`}>
-                <Alert />
-                <p className={styles.subheading}>
-                    Show the world how you look like
+  }
+
+  useEffect(() => {
+    //Runs when component unmounts
+    return () => {
+      setUnMounted(true);
+    }
+  }, []);
+
+
+  if (loading) {
+    return <Loader msg='Authentication in progress' />
+  }
+  return (
+    <div className={styles.cardContainer}>
+      <Card emoji="😎" title={`Hey, ${name}`}>
+        <Alert />
+        <p className={styles.subheading}>
+          Show the world how you look like
                 </p>
-                <div className={styles.avatarContainer} >
-                    <img className={styles.avatarImg} src={image} alt="avatar" />
-                </div>
-                <div>
-                    <input className={styles.avatarInput} type="file" id="avatarInput"
-                        onChange={setSelectedImage}
-                    />
-                    <label className={styles.avatarLabel} htmlFor="avatarInput">Choose your photograph</label>
-                </div>
-                <div class={styles.nextButton}>
-                    <Button onClick={submit} btnText="Next"></Button>
-                </div>
-
-            </Card>
+        <div className={styles.avatarContainer} >
+          <img className={styles.avatarImg} src={image} alt="avatar" />
         </div>
-    )
+        <div>
+          <input className={styles.avatarInput} type="file" id="avatarInput"
+            onChange={setSelectedImage}
+          />
+          <label className={styles.avatarLabel} htmlFor="avatarInput">Choose your photograph</label>
+        </div>
+        <div className={styles.nextButton}>
+          <Button onClick={submit} btnText="Next"></Button>
+        </div>
+
+      </Card>
+    </div>
+  )
 }
 
 export default Avatar;
--- client/src/components/pages/RegisterationSteps/PhoneOrEmail/Email/Email.js
+++ client/src/components/pages/RegisterationSteps/PhoneOrEmail/Email/Email.js
@@ -5,20 +5,20 @@ import TextInput from '../../../../Skeleton/textInputLayout/TextInput'
 import styles from '../PhoneOrEmail.module.css'
 
 const Email = ({ onClick }) => {
-    const [emailId, setemailId] = useState();
-    return (
-        <div>
-            <Card emoji="✉️" title="Enter your Email Id">
-                <TextInput value={emailId} onChange={(e) => setemailId(e.target.value)} />
-                <div class={styles.nextButton}>
-                    <Button onClick={onClick} btnText="Next"></Button>
-                </div>
-                <p>
-                    By entering your email, you’re agreeing to our Terms of Service and Privacy Policy. Thanks!
-                </p>
-            </Card>
+  const [emailId, setemailId] = useState();
+  return (
+    <div>
+      <Card emoji="✉️" title="Enter your Email Id">
+        <TextInput value={emailId} onChange={(e) => setemailId(e.target.value)} />
+        <div className={styles.nextButton}>
+          <Button onClick={onClick} btnText="Next"></Button>
         </div>
-    )
+        <p>
+          By entering your email, you’re agreeing to our Terms of Service and Privacy Policy. Thanks!
+                </p>
+      </Card>
+    </div>
+  )
 }
 
 export default Email;

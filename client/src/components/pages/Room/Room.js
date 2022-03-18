47f88064ee718fc50eb80bf1cdee171421104484
Dhananjay <dhananjayman11@gmail.com>
2022-03-18 12:44:00 +0530

    class error fix

--- client/src/components/pages/Room/Room.js
+++ client/src/components/pages/Room/Room.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Room.module.css';
 
 const Room = () => {
-    return (
+  const [clients, setClients] = useState([
+    {
+      id: 1,
+      name: 'Dj'
+    },
+    {
+      id: 2,
+      name: 'Don J'
+    },
+    {
+      id: 3,
+      name: 'John Doe'
+    }
+  ]);
+
+
+
+  return (
+    <div>
+      <h1>All connected Users</h1>
+      {clients.map(client => (
         <div>
-            SingleRoom
+          <audio controls autoplay></audio>
+          <h4>{client?.name}</h4>
         </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default Room;

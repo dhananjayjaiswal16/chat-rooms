f4ca7a41cfe8a2e75aab6df076e82a13798a98b0
Dhananjay <dhananjayman11@gmail.com>
2022-03-21 00:39:31 +0530

    getting local audio stream

---
+++ client/src/hooks/hookHepler.js
@@ -0,0 +1,15 @@
+export const dummyUsers = [
+  {
+    id: 1,
+    name: 'Dj'
+  },
+  {
+    id: 2,
+    name: 'Don J'
+  },
+  {
+    id: 3,
+    name: 'John Doe'
+  }
+]
+
--- client/src/hooks/useRtc.js
+++ client/src/hooks/useRtc.js
@@ -1,29 +1,41 @@
-import { useRef, useState } from 'react';
+import { useRef, useEffect, useCallback } from 'react';
+import { dummyUsers } from './hookHepler';
 import { useStateWithCallback } from './useStateWithCallback';
 export const useRtc = (roomId, user) => {
   //reason why i created this custom hook was because I wanted a callback function to be triggered after client state has been updated
-  const [clients, setClients] = useStateWithCallback([
-    {
-      id: 1,
-      name: 'Dj'
-    },
-    {
-      id: 2,
-      name: 'Don J'
-    },
-    {
-      id: 3,
-      name: 'John Doe'
-    }
-  ]);
+  const [clients, setClients] = useStateWithCallback([]);
 
   const audioElements = useRef({}); //for storing userId and fetch instance of audioElement for that userId
   const connections = useRef({}); // stroing peerConnections using socketId
   const localMediaStream = useRef(null); //local audio, mic etc data
 
-
   const provideRef = (instance, userId) => {
     audioElements.current[userId] = instance;
   }
+
+  const addNewClient = useCallback((newClient, cb) => {
+    const existingUser = clients.find((client) => client.id === newClient.id) // checks if user already exists in clients
+
+    if (existingUser === undefined) {
+      setClients((otherClients) => ([...otherClients, newClient]), cb)
+    }
+  }, [clients, setClients])
+
+  //Capture Media
+  useEffect(() => {
+    const startCapture = async () => {
+      localMediaStream.current = await window.navigator.mediaDevices.getUserMedia({ audio: true });
+    }
+    startCapture().then(() => {
+      addNewClient(user, () => {
+        const localElement = audioElements.current[user.id];
+        if (localElement) {
+          localElement.volume = 0; //if volume !=0 we will hear our own voice
+          localElement.srcObject = localMediaStream.current;
+        }
+      })
+    })
+  }, [])
+
   return { clients, provideRef };
 }

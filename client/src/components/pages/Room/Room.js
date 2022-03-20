a36c1ac76313daa9fa1cf0feb3c034fefc5a5b19
Dhananjay <dhananjayman11@gmail.com>
2022-03-20 23:17:23 +0530

    custom hook WEB RTC integration with Room component

--- client/src/components/pages/Room/Room.js
+++ client/src/components/pages/Room/Room.js
@@ -1,29 +1,14 @@
 import React, { useState } from 'react'
+import { useRtc } from '../../../hooks/useRtc';
 import styles from './Room.module.css';
 
 const Room = () => {
-  const [clients, setClients] = useState([
-    {
-      id: 1,
-      name: 'Dj'
-    },
-    {
-      id: 2,
-      name: 'Don J'
-    },
-    {
-      id: 3,
-      name: 'John Doe'
-    }
-  ]);
-
-
-
+  const { clients } = useRtc();
   return (
     <div>
       <h1>All connected Users</h1>
-      {clients.map(client => (
-        <div>
+      {clients?.map(client => (
+        <div key={client.id}>
           <audio controls autoplay></audio>
           <h4>{client?.name}</h4>
         </div>
